25a27f0633929afaca4690d6f4e6857ac27994d5
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 12:20:32 +0530

    Update as.py

--- as.py
+++ as.py
@@ -4,9 +4,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, roc_curve, classification_report, auc
 from xgboost import XGBClassifier
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.callbacks import EarlyStopping
-from transformers import BertTokenizer, TFBertModel
-import tensorflow as tf
 from sklearn.decomposition import PCA
