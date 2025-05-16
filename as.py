57e0b371eda492dbbfe277178cc530a53c7cbd25
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 20:41:08 +0530

    Update as.py

--- as.py
+++ as.py
@@ -23,6 +23,7 @@ y = df['isFraud']
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
+from imblearn.over_sampling import SMOTE
 smote = SMOTE(random_state=42)
 X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
 
