884b79fe076acce6b650059d24ae4e5fe6f1390b
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 12:03:11 +0530

    Create as.py

---
+++ as.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, roc_curve, classification_report, auc
+from xgboost import XGBClassifier
