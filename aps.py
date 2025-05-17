27cb4c18979d7edb15489e6dee9f23091a8f0832
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-17 11:00:07 +0530

    Update aps.py

--- aps.py
+++ aps.py
@@ -145,8 +145,7 @@ elif page == "Transaction Checker":
         col1, col2 = st.columns(2)
         
         with col1:
-            step = st.number_input("Hour of Transaction (1-744)", 
-                                  min_value=1, max_value=744, value=10)
+            step = st.number_input("Hour of Transaction (1-744)",list(range(1,744,10)))
             transaction_type = st.selectbox("Transaction Type", 
                                           ["CASH_IN", "CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER"])
             amount = st.number_input("Amount ($)", min_value=0.0, value=1000.0, step=0.01)
